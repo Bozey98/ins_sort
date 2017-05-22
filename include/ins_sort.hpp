00dd4bf3dd72753f315d4032ae4f76aa4b0ef488
Bozey98 <dmusatov98@gmail.com>
2017-05-22 13:28:54 +0300

    Update and rename matrix.hpp to ins_sort.hpp

---
+++ include/ins_sort.hpp
@@ -0,0 +1,37 @@
+#include "stdafx.h"
+#include <vector>
+#include <iostream>
+
+using namespace std;
+template <typename Iterator>
+void ins_sorting(Iterator start, Iterator end)
+{
+	if (start >= end)
+		return;
+	for (Iterator i = start + 1; i != end; ++i)
+	
+		for (Iterator j = i; j != start && *j < *(j - 1); --j)
+		
+			iter_swap(j - 1, j);
+		
+}
+int main()
+{
+	vector <int> test;
+	for (int i = 0; i < 10; i++) {
+		test.push_back(rand() % 100);
+		cout << test[i] << "  ";
+	}
+
+	cout << endl;
+
+	ins_sorting(test.begin(), test.end());
+
+	for (int i = 0; i < 10; i++) {
+		cout << test[i] << "  ";
+	}
+
+	system("pause");
+    return 0;
+}
+
