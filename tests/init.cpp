83c6cea41efb941c9e675aa984eae0de3626e223
Bozey98 <dmusatov98@gmail.com>
2017-06-13 13:22:41 +0300

    Create init.cpp

--- tests/init.cpp
+++ tests/init.cpp
@@ -2,13 +2,13 @@
 #include <catch.hpp>
 SCENARIO("sort","[sort]")
 {
- 	int matrix[6] = { 2 , 1, 6, 3, 5, 4 };
-	int sorted_matrix[6] =  { 1, 2, 3, 4, 5, 6 };
+ 	int matrix[3] = { 1,3,0};
+	int sorted_matrix[3] =  { 0,1,3 };
 	int* first = &matrix[0];
-	int* second = &matrix[6];
+	int* second = &matrix[3];
 	ins_sorting(first, second);
 	bool check = NULL;
-	for (int i = 0; i < 6; i++)
+	for (int i = 0; i < 3; i++)
 	if (matrix[i] == sorted_matrix[i])
 	check = true;
 	REQUIRE(check == true);
